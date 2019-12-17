64c916fc042dd95fa3b949f692924386be12b5e5
Víctor Chico Rodríguez <vchicorod@gmail.com>
2019-12-17 16:19:39 +0100

    Añadidos resultados al código

--- manuales/java/Concatenacion.java
+++ manuales/java/Concatenacion.java
@@ -1,8 +1,8 @@
 class Concatenacion {
     public static void main(String[] args){
-        String firstString = "Esto es";
-        String secondString = " una cadena de texto concatenada.";
-        String thirdString = firstString+secondString;
-        System.out.println(thirdString);
+	String firstString = "Esto es";
+	String secondString = " una cadena de texto concatenada.";
+	String thirdString = firstString+secondString;
+	System.out.println(thirdString);
     }
 }
